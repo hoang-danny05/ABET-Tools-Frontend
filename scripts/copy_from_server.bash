9862b2cb9c72298d4bcb6a971e8aebffeabdf69e
Danny Hoang <bajanboss132@gmail.com>
2026-02-06 16:50:30 -0700

    whoops forgot to save command

--- scripts/copy_from_server.bash
+++ scripts/copy_from_server.bash
@@ -1,1 +1,2 @@
-scp -i .ssh/abet -r osburn@72.167.148.35:/home/osburn/public_html/abet.asucapstonetools.com .
+scp -i .ssh/abet -r osburn@72.167.148.35:/home/osburn/public_html/abet.asucapstonetools.com ..
+scp -i .ssh/abet -r osburn@72.167.148.35:/home/osburn/abet_private ..
